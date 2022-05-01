28822576c950d3f6f6eb2eeb9adf96324ef1f8b5
Gary Fernie <gary.fernie@skyscanner.net>
2022-05-01 19:48:57 +0100

    Move CpeCollector to independent module

---
+++ tests/router_metrics/test_cpe_collector.py
@@ -0,0 +1,89 @@
+import pytest
+from prometheus_client.core import GaugeMetricFamily
+
+from router_metrics import cpe_collector
+
+
+def test_eq():
+    assert cpe_collector.CpeCollector(
+        "foo", "bar", "lol"
+    ) == cpe_collector.CpeCollector("foo", "bar", "lol")
+
+
+def test_not_eq():
+    assert cpe_collector.CpeCollector(
+        "foo", "bar", "lol"
+    ) != cpe_collector.CpeCollector("foo", "bar", "wat")
+
+
+def test_repr():
+    assert (
+        repr(cpe_collector.CpeCollector("foo", "bar", "lol"))
+        == "CpeCollector('foo', username='bar', password='lol')"
+    )
+
+
+def test_repr_kwarg_defaults():
+    assert (
+        repr(cpe_collector.CpeCollector("foo"))
+        == "CpeCollector('foo', username=None, password=None)"
+    )
+
+
+def gauge(name, documentation, labels, label_values: dict):
+    g = GaugeMetricFamily(name, documentation, labels=labels)
+    for k, v in label_values.items():
+        g.add_metric([k], v)
+    return g
+
+
+@pytest.mark.parametrize(
+    "response, expected",
+    (
+        ({}, []),
+        (
+            {"nrdlbandwidth": "100MHz"},
+            [
+                GaugeMetricFamily(
+                    "signal_nrdlbandwidth",
+                    "Signal metrics: nrdlbandwidth",
+                    unit="MHz",
+                    value=100,
+                )
+            ],
+        ),
+        (
+            {"scc_pci": "0x1C3"},
+            [
+                GaugeMetricFamily(
+                    "signal_scc_pci", "Signal metrics: scc_pci", unit="hex", value=451
+                )
+            ],
+        ),
+        (
+            {"transmode": "TM[4]"},
+            [
+                GaugeMetricFamily(
+                    "signal_transmode",
+                    "Signal metrics: transmode",
+                    unit="TM",
+                    value=4,
+                )
+            ],
+        ),
+        (
+            {"nei_cellid": "No1:236No2:234No3:20No4:426"},
+            [
+                gauge(
+                    "signal_nei_cellid",
+                    "Signal metrics: nei_cellid",
+                    labels=["No"],
+                    label_values={"1": 236, "2": 234, "3": 20, "4": 426},
+                )
+            ],
+        ),
+    ),
+    ids=lambda x: str(x) if isinstance(x, dict) else None,
+)
+def test_parse_signal_metrics(response, expected):
+    assert list(cpe_collector.parse_signal_metrics(response)) == expected
--- tests/router_metrics/test_target.py
+++ tests/router_metrics/test_target.py
@@ -1,63 +1,13 @@
-import pytest
-from prometheus_client.core import GaugeMetricFamily
+from unittest import mock
 
 from router_metrics import target
+from router_metrics.cpe_collector import CpeCollector
 
 
-def gauge(name, documentation, labels, label_values: dict):
-    g = GaugeMetricFamily(name, documentation, labels=labels)
-    for k, v in label_values.items():
-        g.add_metric([k], v)
-    return g
-
-
-@pytest.mark.parametrize(
-    "response, expected",
-    (
-        ({}, []),
-        (
-            {"nrdlbandwidth": "100MHz"},
-            [
-                GaugeMetricFamily(
-                    "signal_nrdlbandwidth",
-                    "Signal metrics: nrdlbandwidth",
-                    unit="MHz",
-                    value=100,
-                )
-            ],
-        ),
-        (
-            {"scc_pci": "0x1C3"},
-            [
-                GaugeMetricFamily(
-                    "signal_scc_pci", "Signal metrics: scc_pci", unit="hex", value=451
-                )
-            ],
-        ),
-        (
-            {"transmode": "TM[4]"},
-            [
-                GaugeMetricFamily(
-                    "signal_transmode",
-                    "Signal metrics: transmode",
-                    unit="TM",
-                    value=4,
-                )
-            ],
-        ),
-        (
-            {"nei_cellid": "No1:236No2:234No3:20No4:426"},
-            [
-                gauge(
-                    "signal_nei_cellid",
-                    "Signal metrics: nei_cellid",
-                    labels=["No"],
-                    label_values={"1": 236, "2": 234, "3": 20, "4": 426},
-                )
-            ],
-        ),
-    ),
-    ids=lambda x: str(x) if isinstance(x, dict) else None,
-)
-def test_parse_signal_metrics(response, expected):
-    assert list(target.parse_signal_metrics(response)) == expected
+@mock.patch.dict("os.environ", {"ROUTER_PASSWORD": "foo"})
+@mock.patch("router_metrics.target.REGISTRY")
+def test_register_collectors(mock_registry):
+    target.register_collectors()
+    assert mock_registry.register.mock_calls == [
+        mock.call(CpeCollector("https://hirouter.net", "admin", "foo"))
+    ]
